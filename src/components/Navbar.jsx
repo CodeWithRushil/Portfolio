fe2f51e682b4d56088697355968a899c00b01d7d
Rushil Sharma <workwithrushil@gmail.com>
2025-11-07 18:55:44 +0530

    Change Resume text to a link in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -119,7 +119,7 @@ export default function Navbar() {
                 <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
                 <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
                 <span className="relative text-black group-hover:text-white flex items-center gap-x-3">
-                  Resume <TbDownload size={16} />
+                  <a href="https://drive.google.com/file/d/1GY3NqtNEqV-VvqYL5hI4qNcXmjjm3wbH/view?usp=sharing" target="_blank" className="relative text-black group-hover:text-white flex items-center gap-x-3">Resume</a> <TbDownload size={16} />
                 </span>
               </motion.a>
             </ul>
